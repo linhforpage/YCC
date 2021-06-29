6efba15d6b0b1247a261fc3c6c981318eeaeb4f9
linhforpage <qulinh.forpage@gmail.com>
2021-06-29 23:16:14 +0700

    Load data to homepage

--- YCC.Utilities/Constants/SystemConstants.cs
+++ YCC.Utilities/Constants/SystemConstants.cs
@@ -6,7 +6,7 @@ namespace YCC.Utilities.Constants
 {
     public class SystemConstants
     {
-        public const string MainConnectionString = "eShopSolutionDb";
+        public const string MainConnectionString = "YCCDb";
         public const string CartSession = "CartSession";
 
         public class AppSettings
